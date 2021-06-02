bb3beb1837d296c4e6916c34ca7ffce9cc05b995
prayasgupta1998 <prayas.tension@gmail.com>
2021-06-02 17:15:16 +0530

    prepare for recycler view adapter

--- app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/Controller/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.coderswag.Controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import com.example.coderswag.Adapters.CategoryAdapter
 import com.example.coderswag.Model.Category
 import com.example.coderswag.R
@@ -21,5 +23,8 @@ class MainActivity : AppCompatActivity() {
                 DataService.categories)
 
         categoryListView.adapter = adapter
+
     }
 }
+
+
